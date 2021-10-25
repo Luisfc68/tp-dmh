0e6a187663e319d41b9041d992f2d7cab205af92
Luisfc68 <Luisfc68@gmail.com>
2021-10-25 00:14:52 -0300

    Crea layout de la aplicacion

--- src/layouts/AppLayout.vue
+++ src/layouts/AppLayout.vue
@@ -1,21 +1,20 @@
 <template>
-  <q-layout view="hHh LpR fFf">
+  <q-layout view="hHh lpR fFf">
 
     <q-header reveal elevated class="bg-primary text-white">
       <q-toolbar>
         <q-btn dense flat round icon="menu" @click="toggleLeftDrawer" />
 
-        <q-toolbar-title>
-          <q-avatar>
-            <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg">
-          </q-avatar>
-          Title
+        <q-toolbar-title class='row'>
+          <div class='col self-center q-ml-md'>
+            Chat application TP
+          </div>
         </q-toolbar-title>
       </q-toolbar>
     </q-header>
 
-    <q-drawer v-model="leftDrawerOpen" side="left" overlay elevated>
-      <!-- drawer content -->
+    <q-drawer :width=350 v-model="leftDrawerOpen" side="left" overlay elevated>
+        <ProfileComponent :usuario='usuario' v-on:close='toggleLeftDrawer' />
     </q-drawer>
 
     <q-page-container>
@@ -27,8 +26,18 @@
 
 <script>
 import { ref } from 'vue'
+import ProfileComponent from 'components/user/ProfileComponent.vue'
 
 export default {
+  name: 'AppLayout',
+  components:{
+    ProfileComponent
+  },
+  computed:{
+      usuario: function(){
+          return this.$store.getters['usuario/getUsuario']
+      }
+  },
   setup () {
     const leftDrawerOpen = ref(false)
 
