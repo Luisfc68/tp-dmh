3e9949ad0bb33c20c7f2a15ee527c57005d9b617
Luisfc68 <Luisfc68@gmail.com>
2021-11-01 00:03:37 -0300

    Integra socketio

--- quasar.conf.js
+++ quasar.conf.js
@@ -22,6 +22,8 @@ module.exports = configure(function (ctx) {
     // --> boot files are part of "main.js"
     // https://quasar.dev/quasar-cli/boot-files
     boot: [
+      'axios',
+      'socketio'
     ],
 
     // https://quasar.dev/quasar-cli/quasar-conf-js#Property%3A-css
@@ -94,7 +96,9 @@ module.exports = configure(function (ctx) {
       // directives: [],
 
       // Quasar plugins
-      plugins: []
+      plugins: [
+        'Notify'
+      ]
     },
 
     // animations: 'all', // --- includes all animations
---
+++ src/boot/socketio.js
@@ -0,0 +1,24 @@
+import { boot } from 'quasar/wrappers'
+import VueSocketIO from 'vue-socket.io'
+import SocketIO  from 'socket.io-client'
+
+const socket = SocketIO('ws://ec2-107-20-23-84.compute-1.amazonaws.com',{
+                    reconnection: false,
+                    autoConnect: false,
+                    auth:{
+                        token:''
+                    }
+                })
+
+export default boot(( {app,store} ) =>{
+    new VueSocketIO({
+        debug: true,
+        connection: socket,
+        vuex: {
+            store,
+            actionPrefix: "SOCKET_",
+            mutationPrefix: "SOCKET_"
+          }
+    })
+    app.config.globalProperties.$socket = socket
+})
