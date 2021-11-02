35bd6bd65806c45ddaaa5a35f781644a8be5847a
Luisfc68 <Luisfc68@gmail.com>
2021-11-02 00:50:03 -0300

    Agrega token en todas las peticiones

--- src/boot/axios.js
+++ src/boot/axios.js
@@ -2,10 +2,11 @@
 
 import { boot } from 'quasar/wrappers'
 import axios from 'axios'
+import { GET_TOKEN } from 'src/store/usuario/types'
 
 const api = axios.create({ baseURL: 'http://ec2-107-20-23-84.compute-1.amazonaws.com/tap' })
 
-export default boot(({ app }) => {
+export default boot(({ app,store }) => {
   // for use inside Vue files (Options API) through this.$axios and this.$api
 
   app.config.globalProperties.$axios = axios
@@ -15,6 +16,13 @@ export default boot(({ app }) => {
   app.config.globalProperties.$api = api
   // ^ ^ ^ this will allow you to use this.$api (for Vue Options API form)
   //       so you can easily perform requests against your app's API
+  api.interceptors.request.use(function (config) {
+    const token = store.getters['usuario/'+GET_TOKEN]
+    config.headers.Authorization = 'Bearer '+token
+  
+    return config
+  })
+
 })
 
 export { axios, api }
