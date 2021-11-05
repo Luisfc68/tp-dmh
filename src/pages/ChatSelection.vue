c80fd5e0c64bc9042eac61800e907152bbc01fcc
Luisfc68 <Luisfc68@gmail.com>
2021-11-04 23:10:29 -0300

    Agrega busqueda de chat

--- src/pages/ChatSelection.vue
+++ src/pages/ChatSelection.vue
@@ -1,8 +1,21 @@
 <template>
   <q-page class="flex flex-center">
     <div class='q-pa-md' style='width: 95%'>
-      <div class='full-width row justify-end'>
-        <q-btn round size='sm' color="primary" icon="add" class='q-mb-sm q-mr-lg' @click='crear()'/>
+      <div class='full-width row justify-between'>
+
+        <div class='q-ml-lg q-mb-sm row items-end'>
+          <q-input v-model='query' label='Busca chats (# -> tags)' dense @keyup.enter='pedirQuery()' :disable='searching' >
+            <template v-slot:append>
+              <q-icon v-if="query !== ''" name='close' @click='resetQuery()' class='cursor-pointer' />
+              <q-icon name='search' class='cursor-pointer' @click='pedirQuery()' />
+            </template>
+          </q-input>
+        </div>
+
+        <div class='q-mb-sm q-mr-lg row items-end'>
+          <q-btn round size='sm' color="primary" icon="add" @click='crear()'/>
+        </div>
+
       </div>
       <q-infinite-scroll @load='onLoad' :offset='1' ref='scroll'>
         <q-list>
@@ -42,12 +55,17 @@ export default {
       chats:[],
       creando: false,
       creationMode: true,
-      chatEnviado: null
+      chatEnviado: null,
+      query: '',
+      searching: false
     } 
   },
   methods:{
     onLoad(index, done){
-      this.pedir()
+      if(this.query === '')
+        this.pedir()
+      else
+        this.pedirQuery()
       setTimeout(() => {
         done()
       },2000)
@@ -56,6 +74,27 @@ export default {
       this.$socket.client.emit(CHAT_REQUEST,{
         offset: this.chats.length
       })
+    },
+    parsearQuery(){
+      const title = this.query.split(" ").filter(p => !p.startsWith('#')).join(' ')
+      const tags = this.query.split(" ").filter(p => p.startsWith('#')).map(p => p.substring(1))
+      const parsed = { title,tags }
+      return parsed
+    },
+    pedirQuery(){
+      this.searching = true
+      const query = this.parsearQuery()
+      this.chats = []
+      let busqueda = {
+        offset: this.chats.length,
+        title: query.title,
+        ...(query.tags.length !== 0) && {tags: query.tags}
+      }
+      this.$socket.client.emit(CHAT_REQUEST,busqueda)
+    },
+    resetQuery(){
+      this.query = ''
+      this.refresh()
     },
     refresh(){
       this.chats = []
@@ -75,6 +114,7 @@ export default {
   sockets:{
     [CHAT_REQUEST](data){
       this.chats.push(...data);
+      this.searching = false
       if(data.length === 0)
         this.$refs.scroll.stop()
     }
