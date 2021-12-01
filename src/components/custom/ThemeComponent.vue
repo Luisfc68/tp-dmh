f85795d23932ba6eadbe4109144521bde7c87af9
Luisfc68 <Luisfc68@gmail.com>
2021-12-01 20:18:40 -0300

    Agrega pagina nueva con estilos

---
+++ src/components/custom/ThemeComponent.vue
@@ -0,0 +1,55 @@
+<template>
+    <q-card bordered style="max-width: 300px;" class="q-ma-sm bg-grey-4">
+        <q-card-section class="q-pb-sm">
+            <div class="text-h5">
+                {{titulo}}
+            </div>
+        </q-card-section>
+        <q-card-section class="q-pa-none">
+            <div :class="'bg-'+color" v-for="color in editableColors" :key="color" class="flex justify-center">
+                <q-badge
+                    :ref="color" 
+                    class="q-my-sm text-subtitle2"
+                    color="white"
+                    text-color="black"
+                    :label="colorCode(color)"
+                />
+            </div>
+        </q-card-section>
+        <q-card-actions class="row justify-center">
+            <q-btn flat>Elegir</q-btn>
+            <q-btn flat>Editar</q-btn>
+            <q-btn flat>Eliminar</q-btn>
+      </q-card-actions>
+    </q-card>
+</template>
+<script>
+import { colors } from 'quasar'
+const {getPaletteColor} = colors
+
+export default {
+    name: 'ThemeComponent',
+    data(){
+        return {
+            titulo: 'Test',
+            editableColors: [
+                'primary',
+                'secondary',
+                'accent',
+                'dark',
+                'positive',
+                'negative',
+                'info',
+                'warning',
+                'fondo',
+                'dark-secondary'
+            ]
+        }
+    },
+    computed:{
+        colorCode(){
+            return (color) => getPaletteColor(color)
+        } 
+    }
+}
+</script>
---
+++ src/pages/CustomizationPage.vue
@@ -0,0 +1,25 @@
+<template>
+  <q-page class="flex items-start">
+      <div class="row full-width q-pa-lg justify-center">
+          <div class="col-12 q-px-xl">
+            <q-btn
+              round 
+              size='md' 
+              color="dark" 
+              icon="add" 
+            />
+          </div>
+          <ThemeComponent class="col-sm-3" v-for="n in 10" :key="n"/>
+      </div>
+  </q-page>
+</template>
+<script>
+import ThemeComponent from '../components/custom/ThemeComponent.vue'
+
+export default {
+    name: 'CustomizationPage',
+    components: {
+        ThemeComponent
+    },
+}
+</script>
